598cd367d20f7dc8202ec636e74e1f184d80d2e0
Romario Gustave <romagustave10@gmail.com>
2021-01-06 05:13:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,3 @@
 document.addEventListener("DOMContentLoaded", function() {
   console.log("JavaScript is so cool. It lets me add text to my page programmatically");
 });
- 
-console.log(
-  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
-);
